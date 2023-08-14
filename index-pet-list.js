b45ca395e0deb72966c7be50e788011ed477daf4
abtv23 <aniboteva11@gmail.com>
2023-08-14 23:13:49 +0300

    added pet kinds

--- index-pet-list.js
+++ index-pet-list.js
@@ -1,26 +1,24 @@
 const petsMockData = [{"petId":42,"petName":"Gosho","addedDate":"2022-10-31","kind":1},{"petId":43,"petName":"Pesho","addedDate":"2022-10-25","kind":2},{"petId":44,"petName":"Kenny","addedDate":"2022-10-27","kind":3}]
+const petKindsMockData = [{"displayName":"Parrot","value":3}, {"displayName":"Cat","value":1}, {"displayName":"Dog","value":2}]
 const displayedPets = document.getElementById("pet-list")
-console.log(displayedPets)
 
-
-function renderPetList(pets) { // TODO: rename
+function renderPetList(pets, petKinds) { // TODO: rename
     for(i = 0; i < pets.length; i++) {
         const pet = pets[i]
+        const petKindFound = petKinds.find((item) => {
+            console.log(item, pet)
+            return item.value === pet.kind
+        })     
         const newPetCardElement = document.createElement("div") // TODO: rename
         newPetCardElement.setAttribute("class","pet-card")
         newPetCardElement.setAttribute("id", pet.petId)
         newPetCardElement.innerHTML = `
             <div class="pet-card-name">Name: ${pet.petName}</div> 
-            <div class="pet-card-kind">Kind: ${pet.kind}</div>
+            <div class="pet-card-kind">Kind: ${petKindFound.displayName}</div>
             <div class="pet-card-date">Added Date: ${pet.addedDate}</div>
         `
         displayedPets.appendChild(newPetCardElement)
 
-        console.log(pet)
-        console.log(`Name: ${pet.petName}`)
-        console.log(`Kind: ${pet.kind}`)
-        console.log(`Added Date: ${pet.addedDate}`)
-        
         newPetCardElement.addEventListener('click', renderPetInfoModal)
     }
 }
@@ -44,7 +42,7 @@ function renderPetInfoModal() {
     closePetButton.addEventListener("click", closePetInfoPopup)
 }
 
-renderPetList(petsMockData)
+renderPetList(petsMockData, petKindsMockData)
 
 const addNewPetButton = document.getElementById("add-new-pet-button")
 
